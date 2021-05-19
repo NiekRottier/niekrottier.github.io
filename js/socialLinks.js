4c42368422c4bbea0e84fe253ad029ff61fa3aeb
NiekRottier <niekrottier@gmail.com>
2021-05-19 23:26:37 +0200

    Added social media links

---
+++ js/socialLinks.js
@@ -0,0 +1,8 @@
+// Get elements
+let instaIcon = document.getElementById("instaIcon")
+let linkedInIcon = document.getElementById("linkedInIcon")
+let githubIcon = document.getElementById("githubIcon")
+
+instaIcon.addEventListener("click", () => window.open("https://www.instagram.com/nr.cmgt/"))
+linkedInIcon.addEventListener("click", () => window.open("https://www.linkedin.com/in/niek-rottier-365805176/"))
+githubIcon.addEventListener("click", () => window.open("https://github.com/NiekRottier/"))
