f010e96235fbd5fa257fbe88bc2a54d2cca45ee6
NiekRottier <niekrottier@gmail.com>
2021-06-17 00:36:33 +0200

    Removed log

--- js/projectLinks.js
+++ js/projectLinks.js
@@ -9,7 +9,6 @@ let projects = [
 ]
 
 projects.forEach(project => {
-    console.log("TEST");
     let href = "./" + project + ".html";
     document.getElementById(project).addEventListener("click", () => window.location.href = href)
 });
