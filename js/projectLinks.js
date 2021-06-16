a06977afe54793391334899890464b7ca1de08f4
NiekRottier <niekrottier@gmail.com>
2021-06-17 00:36:18 +0200

    Updated how the links are added to the projects

--- js/projectLinks.js
+++ js/projectLinks.js
@@ -1,15 +1,15 @@
-let urbanJungle = document.getElementById("urbanJungle")
-let jsai = document.getElementById("jsAi")
-let nieuwsRadar = document.getElementById("nieuwsRadar")
-let projectPigeon = document.getElementById("projectPigeon")
-let reactNotes = document.getElementById("reactNotes")
-let backendApi = document.getElementById("backendApi")
-let laravelGallery = document.getElementById("laravelGallery")
+let projects = [
+    "urbanJungle",
+    "jsai",
+    "nieuwsradar",
+    "projectPigeon",
+    "reactNotes",
+    "backendApi",
+    "laravelGallery"
+]
 
-urbanJungle.addEventListener("click", () => window.location.href ="./urbanJungle.html")
-jsai.addEventListener("click", () => window.location.href ="./jsai.html")
-nieuwsRadar.addEventListener("click", () => window.location.href ="./nieuwsradar.html")
-projectPigeon.addEventListener("click", () => window.location.href ="./projectPigeon.html")
-reactNotes.addEventListener("click", () => window.location.href ="./reactNotes.html")
-backendApi.addEventListener("click", () => window.location.href ="./backendApi.html")
-laravelGallery.addEventListener("click", () => window.location.href ="./laravelGallery.html")
+projects.forEach(project => {
+    console.log("TEST");
+    let href = "./" + project + ".html";
+    document.getElementById(project).addEventListener("click", () => window.location.href = href)
+});
